edc6cdf0bbf7df79396f3656f85e8cbea19b720b
Saim Ahmed <sayeemahmed850@gmail.com>
2026-06-15 10:00:12 +0600

    updated base url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://blood-donations-api-1.onrender.com/api",
+  baseURL: "https://manobota-blood-bank-api.onrender.com/api",
 });
 
 api.interceptors.request.use((configs) => {
